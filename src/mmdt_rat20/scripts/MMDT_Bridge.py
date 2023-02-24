de41a41ae0c2fbf73536c4b1023d47cee0211a79
Khronos137 <fernandochicaiza137@gmail.com>
2023-02-23 21:08:53 -0500

    Actualizacion hasta 23-02-2023

--- src/mmdt_rat20/scripts/MMDT_Bridge.py
+++ src/mmdt_rat20/scripts/MMDT_Bridge.py
@@ -15,6 +15,8 @@ global referenceFrame
 global chasisPos
 global chasisVel
 global a                # Distancia desde el CDG hacia el punto de interes
+global identChasis
+global identEndLinkDer
 
 # robotPos = [platfPosX, platfPosY, orient, torsPosZ, torsPosY, manDerQ1..Q6, manIzqQ1..Q6]
 # robotVel = [platfVelX, platfVelY, w, torsVelZ, torsVelY, manDerQ1_p..Q6_p, manIzqQ1_p..Q6_p]
@@ -25,8 +27,11 @@ robotVel = [0.0,0.0,0.0, 0.0,0.0, 0.0,0.0,0.0,0.0,0.0,0.0, 0.0,0.0,0.0,0.0,0.0,0
 chasisPos = [0.0,0.0,0.0]
 chasisVel = [0.0,0.0,0.0]
 gazebo_link_states = 0
-referenceFrame = "mmdt_rat20::chasis"
+referenceFrameChasis = "mmdt_rat20::chasis"
+referenceEndLinkDer = "mmdt_rat20::end_link_manipDer"
 a= 0.2
+identChasis= 10000
+identEndLinkDer= 10000
 
 def quat2eulers(q0, q1, q2, q3):
     """
@@ -68,14 +73,31 @@ def callback_gazeboLinks(data):
     global chasisPos
     global chasisVel
     global a
-    identChasis= 4      # Este es el identificador para seleccionar el comportamiento del robot en el mundo de gazebo
+    global identChasis
+    global identEndLinkDer
 
-    referencia= data.name[identChasis]
-    # print("Lo que lee: ",referencia)
-    # print("Lo que quiero que compare",referenceFrame)
-    # print(referencia==referenceFrame)
+# Identificador del chasis
+    if identChasis==10000:
+        print("longitud", len(data.pose))
+        for kI in range(len(data.pose)):
+            referencia= data.name[kI]
+            if referencia==referenceFrameChasis:
+                print("Referencia de chasis encontrada en: ",kI)
+                identChasis=kI
+                break
+        print("Deberia ser la de chasis: ", data.name[identChasis],identChasis)
 
-    # --- Calculo de las posiciones, orientaciones y velocidades del robot respecto al FRAME movil
+    if identEndLinkDer==10000:
+        print("longitud", len(data.pose))
+        for kI in range(len(data.pose)):
+            referencia= data.name[kI]
+            if referencia==referenceEndLinkDer:
+                print("Referencia de chasis encontrada en: ",kI)
+                identEndLinkDer=kI
+                break
+        print("Deberia ser la de EndLink: ", data.name[identEndLinkDer],identEndLinkDer)
+
+# --- Calculo de las posiciones, orientaciones y velocidades del robot respecto al FRAME movil
     chasisPos_X= data.pose[identChasis].position.x
     chasisPos_Y= data.pose[identChasis].position.y
     quaterW= data.pose[identChasis].orientation.w
@@ -87,6 +109,9 @@ def callback_gazeboLinks(data):
     signo=1
     if orient<0:
         signo=-1
+
+    # print(data.name[identEndLinkDer])
+    # print(data.pose[identEndLinkDer].position)
 
     orient= -signo*(math.pi-signo*orient)
     chasisPos= [chasisPos_X, chasisPos_Y, orient]
@@ -109,6 +134,23 @@ def talker():
     pubIzqF = rospy.Publisher('/mmdt_rat20/jointIzqF_effort/command', Float64, queue_size=10)
     pubIzqT = rospy.Publisher('/mmdt_rat20/jointIzqT_effort/command', Float64, queue_size=10)
 
+    pubBaseTorso = rospy.Publisher('/mmdt_rat20/jointBaseTorso_position/command', Float64, queue_size=10)
+    pubTorso = rospy.Publisher('/mmdt_rat20/jointTorso_position/command', Float64, queue_size=10)
+
+    pubDer1 = rospy.Publisher('/mmdt_rat20/joint1_manipDer_position/command', Float64, queue_size=10)
+    pubDer2 = rospy.Publisher('/mmdt_rat20/joint2_manipDer_position/command', Float64, queue_size=10)
+    pubDer3 = rospy.Publisher('/mmdt_rat20/joint3_manipDer_position/command', Float64, queue_size=10)
+    pubDer4 = rospy.Publisher('/mmdt_rat20/joint4_manipDer_position/command', Float64, queue_size=10)
+    pubDer5 = rospy.Publisher('/mmdt_rat20/joint5_manipDer_position/command', Float64, queue_size=10)
+    pubDer6 = rospy.Publisher('/mmdt_rat20/joint6_manipDer_position/command', Float64, queue_size=10)
+
+    pubIzq1 = rospy.Publisher('/mmdt_rat20/joint1_manipIzq_position/command', Float64, queue_size=10)
+    pubIzq2 = rospy.Publisher('/mmdt_rat20/joint2_manipIzq_position/command', Float64, queue_size=10)
+    pubIzq3 = rospy.Publisher('/mmdt_rat20/joint3_manipIzq_position/command', Float64, queue_size=10)
+    pubIzq4 = rospy.Publisher('/mmdt_rat20/joint4_manipIzq_position/command', Float64, queue_size=10)
+    pubIzq5 = rospy.Publisher('/mmdt_rat20/joint5_manipIzq_position/command', Float64, queue_size=10)
+    pubIzq6 = rospy.Publisher('/mmdt_rat20/joint6_manipIzq_position/command', Float64, queue_size=10)
+
     MMDT_pos = rospy.Publisher('MMDT_pos', Float64MultiArray, queue_size=10)
     MMDT_vel = rospy.Publisher('MMDT_vels', Float64MultiArray, queue_size=10)
 
@@ -119,7 +161,7 @@ def talker():
     robotPositions = Float64MultiArray()
     robotVelocities = Float64MultiArray()
 
-    rate = rospy.Rate(100) # 10hz
+    rate = rospy.Rate(200) # 10hz
     print('Ejecutando...')
     while not rospy.is_shutdown():       
         # Aplicacion de comandos sobre el robot en GAZEBO
@@ -131,6 +173,37 @@ def talker():
         pubDerT.publish(ruedaDerT)
         pubIzqF.publish(ruedaIzqF)
         pubIzqT.publish(ruedaIzqT)
+
+        BaseTorso= frameCommands[4]
+        Torso= frameCommands[5]
+        pubBaseTorso.publish(BaseTorso)
+        pubTorso.publish(Torso)
+
+        der1= frameCommands[6]
+        der2= frameCommands[7]
+        der3= frameCommands[8]
+        der4= frameCommands[9]
+        der5= frameCommands[10]
+        der6= frameCommands[11]
+        pubDer1.publish(der1)
+        pubDer2.publish(der2)
+        pubDer3.publish(der3)
+        pubDer4.publish(der4)
+        pubDer5.publish(der5)
+        pubDer6.publish(der6)
+
+        izq1= frameCommands[12]
+        izq2= frameCommands[13]
+        izq3= frameCommands[14]
+        izq4= frameCommands[15]
+        izq5= frameCommands[16]
+        izq6= frameCommands[17]
+        pubIzq1.publish(izq1)
+        pubIzq2.publish(izq2)
+        pubIzq3.publish(izq3)
+        pubIzq4.publish(izq4)
+        pubIzq5.publish(izq5)
+        pubIzq6.publish(izq6)
 
         # Publicacion de informacion para consumir en MATLAB    
         # print(frameCommands)
